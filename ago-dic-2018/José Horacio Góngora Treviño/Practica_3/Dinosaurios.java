faf8e93a0d4069b421032e92e151943034bf9cf3
unknown <josehgt12@live.com.mx>
2018-09-08 17:52:57 -0500

    D fantasma Corregido

--- ago-dic-2018/José Horacio Góngora Treviño/Practica_3/Dinosaurios.java
+++ ago-dic-2018/José Horacio Góngora Treviño/Practica_3/Dinosaurios.java
@@ -4,37 +4,21 @@ public class Dinosaurios
 	public static void main(String []args)
 	{
 		Scanner s = new Scanner(System.in);		
-		String[] a = new String[100];
 		String lista,r="";
 		int n=0,j=0;
 		
 		System.out.println("Nombres de los dinosaurios separados con un espacio:");
 		lista=s.nextLine();
 		 
-		for(int i=0;i<lista.length();i++)
-		{
-			n=i;
-			String d="";
-			
-			while(n!=lista.length()&&lista.charAt(n)!=' ')
-			{
-				d+=lista.charAt(n);
-				n++;
-			}
-			
-			a[j]=d;
-			d="";
-			j++;
-			i=n;
-		}
-		
+		String a[]= lista.split(" ");
+
 		for(int i=0;i<a.length;i++)
 		{
 			int min=i;
 			int g=i;
 			String temp;
 			
-			while(a[g]!=null&&g<a.length)
+			while(g<a.length&&a[g]!=null)
 			{
 				if(a[g].length()<a[min].length())
 				{
