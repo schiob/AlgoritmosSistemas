baba2b8d91a61dfd0bbfeb936c4675b85848b6f5
Jahar <jahirps98@outlook.com>
2018-09-14 18:48:43 -0500

    Practica 4

---
+++ ago-dic-2018/Pérez Sánchez José Jahir/Practica 4/Libro.java
@@ -0,0 +1,31 @@
+
+package algoritmos;
+
+class Libro {
+    String name;
+    int pages;
+
+    public Libro(String name, int pages) {
+        this.name = name;
+        this.pages = pages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+   
+    
+}
