bf80e7d88522fa341444f6757ee092aade9570aa
Mario <mariomanza150@gmail.com>
2019-01-18 22:58:03 -0600

    nuevo archivo practica 1

---
+++ ene-jun-2019/Mario Manzano/Practica_1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Practica_1 {
+  public static void main(String[]args) {
+    Scanner s = new Scanner(System.in);
+    int a = s.nextInt();
+		int b = s.nextInt();
+		int x = a + b;
+		System.out.println(x);
+  }
+}
