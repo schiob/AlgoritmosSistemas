58482b31881dfc8b6bfa5eecb5acbcc02b5c323f
black994 <47092546+black994@users.noreply.github.com>
2019-01-28 18:10:03 -0600

    practica1

---
+++ ene-jun-2019/Juan Pablo Sarmirento Cervera/Practicas/practica1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Practica1
+{
+    public static void main(String args[])
+     {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        int num2 = s.nextInt();
+        System.out.println(" "+(num+num2));
+        }
+}
