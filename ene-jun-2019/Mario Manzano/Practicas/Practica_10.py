314d89a1868c5396ae954d716221e3b046750cc9
Mario <mariomanza150@gmail.com>
2019-05-06 21:30:45 -0500

    Practica_10

---
+++ ene-jun-2019/Mario Manzano/Practicas/Practica_10.py
@@ -0,0 +1,49 @@
+def perm(iterable, r=None):
+    pool = tuple(iterable)
+    n = len(pool)
+    r = n if r is None else r
+    if r > n:
+        return
+    indices = list(range(n))
+    cycles = list(range(n, n-r, -1))
+    yield tuple(pool[i] for i in indices[:r])
+    while n:
+        for i in reversed(range(r)):
+            cycles[i] -= 1
+            if cycles[i] == 0:
+                indices[i:] = indices[i+1:] + indices[i:i+1]
+                cycles[i] = n - i
+            else:
+                j = cycles[i]
+                indices[i], indices[-j] = indices[-j], indices[i]
+                yield tuple(pool[i] for i in indices[:r])
+                break
+        else:
+            return
+
+def comb(iterable, r):
+    pool = tuple(iterable)
+    n = len(pool)
+    if not n and r:
+        return
+    indices = [0] * r
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != n - 1:
+                break
+        else:
+            return
+        indices[i:] = [indices[i] + 1] * (r - i)
+        yield tuple(pool[i] for i in indices)
+
+n, m, x = list(map(int, input().split()))
+a = [e for e in range(1,m+1)]
+c = 0
+
+for e in comb(a,n):
+    print(e,sum(e),len(set(perm(e))))
+    if sum(e) == x:
+        c += len(set(perm(e)))
+
+print(c)
