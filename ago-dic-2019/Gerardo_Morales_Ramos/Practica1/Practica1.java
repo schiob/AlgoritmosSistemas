a96d26d27a65ce6fd70ac71537398678cfc1feaf
gera9 <gerardo_9978@outlook.com>
2019-09-04 11:14:30 -0500

    Practica1

--- ago-dic-2019/Gerardo_Morales_Ramos/Practica1/Practica1.java
+++ ago-dic-2019/Gerardo_Morales_Ramos/Practica1/Practica1.java
@@ -1,12 +1,23 @@
-package practica1;
+package P1;
 import java.util.Scanner;
 
 public class Practica1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] numeros = {3, 43, 2, 5, 4, 6, 5, 4, 334, 45, 23, 123, 323, 45, 34, 23, 43, 5, 46, 78};
-        for (int i = (numeros.length-1); i >= 0; i--){
+        
+        System.out.println("Digite el total números a ingresar: ");
+        int cant = sc.nextInt();
+        
+        int[] numeros = new int[cant];
 
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Digite el número " + (i + 1));
+            numeros[i] = sc.nextInt();
+            System.out.println("");
+        }
+       
+        System.out.println("Salida: ");
+        for (int i = (numeros.length-1); i >= 0; i--){
             if((numeros[i] % 2) == 0){
                     System.out.print(numeros[i] + " "); 
             }
