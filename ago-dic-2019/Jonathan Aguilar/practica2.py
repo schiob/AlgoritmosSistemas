7b7b97aaf50a1a97d61c1087d54e9a40aec07e6f
jonathan-aguilar <jonathan.mushu@gmail.com>
2019-09-07 18:14:34 -0500

    practica2

---
+++ ago-dic-2019/Jonathan Aguilar/practica2.py
@@ -0,0 +1,20 @@
+a=[]
+b=int(input("Dame la cantidad de problemas: "))
+R=int(input("Dame los minutos disponibles que tienes:" ))
+for F in range (b):
+	u=int(input())
+	a.append(u)	
+for F in range (1, len(a)):
+	z= a[F]
+	j= F-1
+	while j>=0 and z<a[j]:
+		a[j+1]=a[j]
+		j=j-1
+	a[j+1]=z
+count=0
+
+for F in range(b):
+	if R>=a[F] and R>=0:
+		count=count+1
+		R=R-a[F]
+print("cuantos problemas puedes hacer en ese tiempo: ", count)
