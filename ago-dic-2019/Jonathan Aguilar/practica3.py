328b4827e92905e9475439723e82b9664cd137db
jonathan-aguilar <jonathan.mushu@gmail.com>
2019-09-18 14:17:02 -0500

    pracica 3 corregida uwu

--- ago-dic-2019/Jonathan Aguilar/practica3.py
+++ ago-dic-2019/Jonathan Aguilar/practica3.py
@@ -1,3 +1,10 @@
 a=list(input("Dame la ista con los nombres de dinosaurios: ").split())
-for p in sorted(a, key=len):
-	print(p , end=", ")
+for F in range (1, len(a)):
+	z= a[F]
+	j= F-1
+	while j>=0 and len(z)<len(a[j]):
+		a[j+1]=a[j]
+		j=j-1
+	a[j+1]=z
+
+print(" ".join(a))
