7df7b213e622c997cf5d8f9ebde5c649f5d1053b
jorge <hernandez.jorge@uadec.edu.mx>
2019-09-18 13:57:53 -0500

    Practica 3 Corregida

--- ago-dic-2019/Jorge de Jesus Hernandez Vazquez/Practicas/practica3.py
+++ ago-dic-2019/Jorge de Jesus Hernandez Vazquez/Practicas/practica3.py
@@ -1,3 +1,13 @@
-L = list(map(str, input("Manuel,dame los nombres porfa: ").split()))
-for p in sorted(L, key=len):
-    print(p, end=" ")
+L = list(map(str, input("Maldita sea manuel, por lo menos dame los nombres: ").split()))
+def selectionSort(L):
+    for i in range(len(L)):
+        indxmin = i
+        for j in range(i+1, len(L)):
+            if len(L[j]) < len(L[indxmin]):
+                indxmin = j
+                #print(' '.join(L)) Para ver como va ordenando#
+        t = L[i]
+        L[i] = L[indxmin]
+        L[indxmin] = t 
+selectionSort(L)
+print(' '.join(L), end="")
