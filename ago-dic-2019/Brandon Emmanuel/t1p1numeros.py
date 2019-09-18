235a110eb7a19a407fbfc5252e2e7304a40d1fa5
Andremm303 <brandondelabra303@gmail.com>
2019-09-17 22:41:57 -0500

    corregido

--- ago-dic-2019/Brandon Emmanuel/t1p1numeros.py
+++ ago-dic-2019/Brandon Emmanuel/t1p1numeros.py
@@ -1,13 +1,12 @@
 array1= []
 array2= []
 
-print("infresa numeros ")
-for i in range (1,6):
+print("ingresa numeros ")
+for i in range (1,21):
     n=int(input())
     array1.append(n)
     if (n %2==0):
          array2.append(n)
-
 
 print("numeros",array1)
 print("numeros pares ",array2)
@@ -17,7 +16,7 @@ print("orden inverso", array2)
 #apend va agregando al ultimo
 #reverse, imprime al reves
 
-
+# big o se enfoca en el peor de los casos
 
 
 
