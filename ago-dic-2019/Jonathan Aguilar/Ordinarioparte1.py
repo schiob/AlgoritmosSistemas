82336d3a6b17e123af04a0d8fc757e8d5ad901fe
jonathan-aguilar <jonathan.mushu@gmail.com>
2019-11-26 00:12:29 -0600

    Ordinario parte 1

--- ago-dic-2019/Jonathan Aguilar/Ordinarioparte1.py
+++ ago-dic-2019/Jonathan Aguilar/Ordinarioparte1.py
@@ -1,10 +1,43 @@
-n=input()
-arreglo=[]
+def mergeSort(arr,rareza):
+    if len(arr) >1:
+        medio = len(arr)//2
+        Izq = arr[:medio]
+        der = arr[medio:]
+        mergeSort(Izq,rareza)
+        mergeSort(der,rareza)
+  
+        i =0 
+        j = 0
+        k = 0
+        while i < len(Izq) and j < len(der):
+            if rareza[Izq[i][1]] < rareza[der[j][1]]:
+                arr[k] = Izq[i]
+                i+=1
+            elif rareza[Izq[i][1]] == rareza[der[j][1]] and Izq[i][0] < der[j][0]:
+                arr[k] = Izq[i]
+                i+=1
+            else:
+                arr[k] = der[j]
+                j+=1
+            k+=1
 
+        while i < len(Izq):
+            arr[k] = Izq[i]
+            i+=1
+            k+=1
+        while j < len(der): 
+            arr[k] = der[j] 
+            j+=1
+            k+=1
 
-for i in range(int(n)): 
-    rareza={"común":1, "especial":2, "épica":3, "legendaria":4}
-    N = input().split()
-    mano=N[:1]
-    arreglo.append(mano)
-print(arreglo)
+rareza = {"común":1, "especial":2, "épica":3, "legendaria":4}
+
+cartas = []
+
+for i in range(int(input())):
+    cartas.append(input().split())
+
+mergeSort(cartas,rareza)
+print("====orden===")
+for i in cartas:
+    print(i[0])
