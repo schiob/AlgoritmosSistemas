c24446a1c26f72795897b53c014271432758fc39
jovanny <ppjovaz@gmail.com>
2020-01-24 21:21:05 -0600

    practica 1

---
+++ ene-jun-2020/JoseJovannyFloresReyna/practica1/practica1.java
@@ -0,0 +1,22 @@
+package parcial1;
+
+import java.util.Scanner;
+
+public class practica1 {
+	public static void main (String []args){
+		int x, y;
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Ingrese valor de x<=-100: ");
+		x=entrada.nextInt();
+		
+		System.out.print("Ingrese valor de y<=100: ");
+		y=entrada.nextInt();
+		
+		System.out.println(x+y);
+		
+		
+		
+	}
+
+}
