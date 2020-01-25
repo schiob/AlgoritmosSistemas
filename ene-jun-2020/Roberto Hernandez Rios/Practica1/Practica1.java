db658086b4178de1dd055c98b49e581301d9d3d4
roberto <trueque.17@gmail.com>
2020-01-24 20:09:10 -0600

    Practica 1

---
+++ ene-jun-2020/Roberto Hernandez Rios/Practica1/Practica1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadores;
+
+/**
+ *
+ * @author Roberto
+ */
+import java.util.Scanner;
+public class Practica1 {
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        int x,y;
+        x = entrada.nextInt();
+        y = entrada.nextInt();
+        if (x>=-100) {
+            
+        }
+        if (y<=100) {
+            
+        }
+        System.out.println(x+y);
+    }
+}
