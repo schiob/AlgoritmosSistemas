db49d1d330de6a7199ed023a25f473474c53caec
alicia-granados <aliciagranados@uadec.edu.mx>
2020-01-29 16:29:25 -0600

    practica 1

---
+++ ene-jun-2020/alicia_granados/practica1/PRACTICAA1.py
@@ -0,0 +1,9 @@
+a=input().split(" ")
+#print( arreglo)
+#a=['1','2','3','-4']
+b= [int(x) for x in a]
+print(b)
+suma=0
+for i in range (0,len(b)):
+ suma+=int(b[i])
+
---
+++ ene-jun-2020/alicia_granados/practica1/practica1.py
@@ -0,0 +1,10 @@
+a=input().split(" ")
+#print( arreglo)
+#a=['1','2','3','-4']
+b= [int(x) for x in a]
+print(b)
+suma=0
+for i in range (0,len(b)):
+ suma+=int(b[i])
+
+print (suma)
