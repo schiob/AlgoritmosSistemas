d3a9f8a6bb84d3dd989d76041cf1883c62c9323e
unknown <victor.523678@gmail.com>
2020-01-29 17:35:19 -0600

    Practica 1 Victor De Leon

---
+++ ene-jun-2020/VictorDeLeon/practica1/Practica1.java
@@ -0,0 +1,15 @@
+package algoritmosdord;
+import java.util.*;
+public class Practica1 {
+    public static void main(String[] args) {
+    Scanner s= new Scanner(System.in);
+    int x, y;
+        System.out.println("Ingresa los numeros enteros X y Y");
+        x=s.nextInt();
+        y=s.nextInt();
+        int res = x + y;
+        System.out.println(res);
+    
+    }
+    
+}
