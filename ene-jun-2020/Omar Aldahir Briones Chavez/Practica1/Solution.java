7c846777446a2cedf3e4be0be6f37b0017bc196b
Aldahir <alda_saya@hotmail.com>
2020-01-29 17:52:31 -0600

    A1

---
+++ ene-jun-2020/Omar Aldahir Briones Chavez/Practica1/Solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static void main(String[] args) 
+    {
+        Scanner f = new Scanner(System.in);
+        int A = f.nextInt();
+        int B = f.nextInt();
+        
+        int Suma = (A + B);
+        System.out.println(Suma);
+        
+    }
+}
