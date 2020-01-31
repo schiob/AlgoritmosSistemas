4229dab06cfabe5d48fa4c1dbfd016bb927239a4
uriel <uri333link@gmail.com>
2020-01-31 17:28:46 -0600

    Practica1

---
+++ ene-jun-2020/Jesus Uriel Moncada Lara/Tarea1/practica1.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+       
+        String n = Console.ReadLine();
+
+        for (int i = 0; i < 1; i++)
+        {
+            string[] arreglo = n.Split();
+            int a = int.Parse(arreglo[0]);
+            int b = int.Parse(arreglo[1]);
+            Console.WriteLine(a + b);
+        }
+
+
+
+    }
+}
