9b0228739aa49d4ad2566e887d2dbfe36626950b
Gonchologon <gonchologon2@gmail.com>
2020-02-14 16:47:23 -0600

    Practica 3, Medicamentos (Gonzalo de jesus garcia sanchez)

---
+++ ene-jun-2020/Gonzalo de Jesus Garcia Sanchez/programa3-Bien-echo.py
@@ -0,0 +1,28 @@
+def bubbleSort(cadenas):
+    for i in range(len(cadenas)):
+        for j in range (1, len(cadenas)-i):
+            if len(cadenas[j-1]) > len(cadenas[j]):
+                temp = cadenas[j-1]
+                cadenas[j - 1] = cadenas[j]
+                cadenas[j] = temp
+
+# imprime en pantalla los valores de un arreglo
+def printArray(arr):
+     for i in range(len(arr)):
+         print(arr[i])
+
+#cadenas = ["CANDESARTAN HIDROCLOROTIAZIDA ALTER","CARBAMAZEPINA NORMON","MATRIF","ACICLOVIR SANDOZ","MELOXICAM","FENDIVIA","SEBUMSELEN"]
+
+n = input("Numero de medicamentos: ") #pedir número de medicamentos
+cadenas = []
+# pedir nombre de cada medicamento
+for i in range(int(n)):
+    cadenas.append(input(str(i + 1) + ".-"))
+
+print("\nLista ordenada:")
+bubbleSort(cadenas) #ordena los medicamentos
+printArray(cadenas) #lista las palabras
+
+# print(len(cadena[2]))
+# print(a)
+#CANDESARTAN HIDROCLOROTIAZIDA ALTER CARBAMAZEPINA NORMON MATRIF ACICLOVIR SANDOZ MELOXICAM FENDIVIA SEBUMSELEN
