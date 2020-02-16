62009c8b053b172ea85a81a3961a26ab1380e1ff
alicia-granados <aliciagranados@uadec.edu.mx>
2020-02-15 21:38:58 -0600

    practica 3

---
+++ ene-jun-2020/alicia_granados/practica3/practica3.py
@@ -0,0 +1,11 @@
+def ordenamientoBurbuja(unaLista):
+    for numPasada in range(len(unaLista)-1,0,-1):
+        for i in range(numPasada):
+            if unaLista[i]>unaLista[i+1]:
+                temp = unaLista[i]
+                unaLista[i] = unaLista[i+1]
+                unaLista[i+1] = temp
+
+unaLista = [54,26,93,17,77,31,44,55,20]
+ordenamientoBurbuja(unaLista)
+print(unaLista)
