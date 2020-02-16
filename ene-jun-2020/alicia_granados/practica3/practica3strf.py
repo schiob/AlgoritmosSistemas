e50b296d6bc61cba6007b7170c30b684a12ca6f7
alicia-granados <aliciagranados@uadec.edu.mx>
2020-02-15 21:54:50 -0600

    practica 3

---
+++ ene-jun-2020/alicia_granados/practica3/practica3strf.py
@@ -0,0 +1,30 @@
+
+n=int(input())
+arreglo=[]
+
+#ingresa
+for i in range (int (n)):
+    arreglo.append(input(str (i)))
+
+#ordena 
+def selectionSort(L):
+    for i in range(len(L)):
+        indxmin = i
+        for j in range(i+1, len(L)):
+            if len(L[j]) < len(L[indxmin]):
+                indxmin = j
+                #Para ver como va ordenando#
+        t = L[i]
+        L[i] = L[indxmin]
+        L[indxmin] = t 
+selectionSort(arreglo)
+
+#imprime
+for i in range (int (n)):
+    print(arreglo[i])
+
+
+
+
+
+ 
