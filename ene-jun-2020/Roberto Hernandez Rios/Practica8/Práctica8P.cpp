b730c6814e9d8754c607fa72209869b4e91543b5
Roberto Hernandez <hernandez_roberto@uadec.edu.mx>
2020-05-25 17:01:47 -0500

    Practica 8

---
+++ ene-jun-2020/Roberto Hernandez Rios/Practica8/Práctica8P.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+
+	int matriz[15][50], n,repisa, libro, pagina,posicion,b;
+	
+	for(int i=0; i<15; i++){
+		for(int j=0; j<50; j++){
+			cin>>matriz[i][j];
+			char c=getchar();
+		}
+	}
+	
+	cin>>n;	
+	
+	for(int i=0; i<15; i++){
+		for(int j=0; j<50; j++){
+			if(n == matriz[i][j]){
+				repisa = (i/5)+1;
+				libro = (i%5)+1; 
+				pagina = (j/5)+1;
+				posicion = (j%5)+1;
+				
+				cout<<repisa<<' '<<libro<<' '<<pagina<<' '<<posicion<<'\n';
+				return(0);						
+			}
+		}
+	}
+	cout<<"-1\n";
+
+		
+	return (0);
+}
