89dd0cb4b981b7d98822fd60e4dae8da964b4370
LucelyFlores <65925166+LucelyFlores@users.noreply.github.com>
2020-09-04 19:34:52 -0500

    LucelyFlores

---
+++ ago-dic-2020/LucelyFlores/Practica1/Práctica1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Práctica1 
+{	public static void main(String[] args) 
+	{	int A, B;
+	    Scanner sc= new Scanner (System.in);
+	    A= sc.nextInt();
+	    B= sc.nextInt();
+	    int suma= A + B;
+	    System.out.println(suma);
+	}
+}
