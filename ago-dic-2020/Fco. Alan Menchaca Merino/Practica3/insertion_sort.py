8d17c6a7c78a6a70f2e46c1662460c65fd5bac3c
Fco. Alan Menchaca M <alan_menchaca22@gero.us>
2020-09-18 21:28:53 -0500

    add insertion sort to practice3

---
+++ ago-dic-2020/Fco. Alan Menchaca Merino/Practica3/insertion_sort.py
@@ -0,0 +1,37 @@
+class InsertionSort:
+
+    def sort(self, array):
+        for i in range(0, len(array) - 1):
+            pos_actual = i
+            pos_siguiente = (pos_actual + 1)
+
+            while array[pos_siguiente] <= array[pos_actual]:
+                elemento_actual = array[pos_actual]
+                elemento_siguiente = array[pos_siguiente]
+
+                array[pos_actual] = elemento_siguiente
+                array[pos_siguiente] = elemento_actual
+
+                if pos_actual == 0:
+                    break
+
+                pos_actual -= 1
+                pos_siguiente = (pos_actual + 1)
+
+    def reverse_sort(self, array):
+        for i in range(0, len(array) - 1):
+            pos_actual = i
+            pos_siguiente = (pos_actual + 1)
+
+            while array[pos_siguiente] >= array[pos_actual]:
+                elemento_actual = array[pos_actual]
+                elemento_siguiente = array[pos_siguiente]
+
+                array[pos_actual] = elemento_siguiente
+                array[pos_siguiente] = elemento_actual
+
+                if pos_actual == 0:
+                    break
+
+                pos_actual -= 1
+                pos_siguiente = (pos_actual + 1)
