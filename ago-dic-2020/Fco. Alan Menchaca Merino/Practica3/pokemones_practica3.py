7d47ecbd148a18b9715a0e1291b5f117bf185a0c
Fco. Alan Menchaca M <alan_menchaca22@gero.us>
2020-09-28 22:58:13 -0500

    pokemones_practica3.py updated

--- ago-dic-2020/Fco. Alan Menchaca Merino/Practica3/pokemones_practica3.py
+++ ago-dic-2020/Fco. Alan Menchaca Merino/Practica3/pokemones_practica3.py
@@ -1,12 +1,9 @@
-from insertion_sort import InsertionSort
+from algoritmos.insertion_sort import InsertionSort
 
 pokemon_cplist = input("Ingresa el poder de cada pokemon (CP): ").split()
 pokemon_list = [int(cp) for cp in pokemon_cplist]
 
-InsertionSort().reverse_sort(pokemon_list)
+InsertionSort.reverse_sort(pokemon_list)
 
-msg = 'Ordenados de mayor a menor: '
-for pokemon in pokemon_list:
-    msg += f"%d, " % pokemon
-
-print(msg)
+print('Ordenados de mayor a menor: ', end="")
+asd = [print(pokemon, end=", ") for pokemon in pokemon_list]
