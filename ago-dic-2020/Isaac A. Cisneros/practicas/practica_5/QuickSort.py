f5e5d7fe92426d251d0f2c76101a3edaf0803f0f
IsaacElCrack <isaac56talk@gmail.com>
2020-10-12 14:23:19 -0500

    Practica 5 Isaac A. Cisneros

---
+++ ago-dic-2020/Isaac A. Cisneros/practicas/practica_5/QuickSort.py
@@ -0,0 +1,29 @@
+def partition(arr, low, high):
+	i = (low-1)	
+	pivot = arr[high]	 
+
+	for j in range(low, high):
+
+		if arr[j] <= pivot:
+
+			i = i+1
+			arr[i], arr[j] = arr[j], arr[i]
+
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return (i+1)
+def quickSort(arr, low, high):
+	if len(arr) == 1:
+		return arr
+	if low < high:
+
+		pi = partition(arr, low, high)
+		quickSort(arr, low, pi-1)
+		quickSort(arr, pi+1, high)
+
+arr = [10, -7, 8, 9, -1.34, 5, -1.32]
+n = len(arr)
+quickSort(arr, 0, n-1)
+print("Lista ordenada es:")
+for i in range(n):
+	print(arr[i]),
+
