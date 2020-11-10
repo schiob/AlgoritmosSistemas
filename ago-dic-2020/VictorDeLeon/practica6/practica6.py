31dbf3ca82a948030f77fb050374afcf23a9a536
unknown <victor.523678@gmail.com>
2020-11-09 20:46:41 -0600

    Practica 6 victor de leon

---
+++ ago-dic-2020/VictorDeLeon/practica6/practica6.py
@@ -0,0 +1,16 @@
+def ord(medicina):
+    sorted(medicina.split(),key=lambda palabra:len(palabra),reverse=True)
+ 
+
+entrada = input().split
+buscar = int(entrada[1])
+medicina = [input()]
+
+for i in range(buscar):
+    med = input()
+    if med in medicina:
+        return posicion
+    
+   
+ord(arr)
+print(" ".join(x[0] for x in arr))
