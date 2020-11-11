400ed07e1973c71f066eacfb9c18d4648d476d69
LuisEnriqueCM <hankku.droon@gmail.com>
2020-11-11 01:56:40 -0600

    Mas vale tarde que nunca :3

---
+++ ago-dic-2020/Luis Enrique Cázares Martínez/Practica 5/Practica5.py
@@ -0,0 +1,22 @@
+def sort(lista):
+    izquierda = []
+    centro = []
+    derecha = []
+    if len(lista) > 1:
+        pivote = lista[0]
+        for i in lista:
+            if i < pivote:
+                izquierda.append(i)
+            elif i == pivote:
+                centro.append(i)
+            elif i > pivote:
+                derecha.append(i)
+        #print(izquierda+["-"]+centro+["-"]+derecha)
+        return sort(izquierda)+centro+sort(derecha)
+    else:
+      return lista
+
+n = int(input("Dime cuantas palabras son -> "))
+palabras = input("Dime la lista de pakabras de Ricardo separadas por un espacio: ")
+palabras = palabras.split(" ")
+print(sort(palabras))
