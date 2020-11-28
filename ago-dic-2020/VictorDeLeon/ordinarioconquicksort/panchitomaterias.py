7e62cd5caaa58c65ab1497b74d929b147f4009f8
unknown <victor.523678@gmail.com>
2020-11-27 21:24:41 -0600

    Profe no lo habia echo con quickSort soy Victor De Leon pero ya quedo

---
+++ ago-dic-2020/VictorDeLeon/ordinarioconquicksort/panchitomaterias.py
@@ -0,0 +1,56 @@
+def quickSort(arr,inicio_idx,fin_idx):
+    if inicio_idx >= fin_idx:
+        return
+
+    
+    pivote_idx = inicio_idx
+    compara = fin_idx
+
+    while compara != pivote_idx:
+        if ord[compara] > ord[pivote_idx]:
+            if arr[compara] <= arr[pivote_idx]:
+                #intercambio
+                temp = arr[pivote_idx]
+                arr[pivote_idx] = arr[compara]
+                arr[compara] = temp
+
+                #intercambio indice
+                temp_idx = pivote_idx
+                pivote_idx = compara
+                copara = temp_idx
+
+        else:
+             if ord(arr[compara]) > ord(arr[pivote_idx]):
+                #intercambio
+                temp = arr[pivote_idx]
+                arr[pivote_idx] = arr[compara]
+                arr[compara] = temp
+
+                #intercambio indice
+                temp_idx = pivote_idx
+                pivote_idx = compara
+                copara = temp_idx
+
+        if compara > pivote_idx:
+            compara -= 1
+        else:
+            compara += 1
+            
+
+    quickSort(arr,inicio_idx,pivote_idx-1)
+    quickSort(arr,pivote_idx+1,fin_idx)
+
+entrada = int(input())
+arr = []
+for i in range(entrada):
+    materia = input().split()
+    arr.append([materia[1]])
+
+
+
+inicio = 0
+
+print(arr)
+quickSort(arr, inicio, len(arr)-1)
+print(arr)
+print(" ".join(x[0] for x in arr))
