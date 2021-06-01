51e0690f415c8285d646ad1f2446f46993d74c83
Yahaira-0421 <yahairamoreno416@gmail.com>
2021-05-31 21:01:39 -0500

    ORDINARIO PARACTICA BUSQUEDA

---
+++ ene-jun-2021/Yahaira Moreno/BUSQUEDA1.java
@@ -0,0 +1,21 @@
+public class BusquedaSecuencial {
+	public static void main(String[] args) {
+		int[] lista = new int[5 Indigo-0,
+Rojo-43,
+Anaranjado-34,
+Anaranjado-44,
+Indigo-0,
+Indigo-3];	
+		boolean encontrado = false;	
+		int i = -1;			
+		
+		while (1<n<500) && !encontrado )
+			if (lista[i++] == n)		
+				encontrado = true;
+
+		if (encontrado)
+			System.out.println("Sí hay algún valor en la lista.");
+		else
+			System.out.println("No hay ningún valor en la lista.");
+	}
+}
