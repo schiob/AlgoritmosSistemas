364a0e24214e11f173267b82317e823b61cd06c4
juancr <lobosgx@hotmail.com>
2021-08-27 20:25:01 -0500

    se modifico la practica

--- ago-dic-2021/Juan Antonio Cruz Perez/practica 1/Practica1.java
+++ ago-dic-2021/Juan Antonio Cruz Perez/practica 1/Practica1.java
@@ -8,16 +8,13 @@ public class Practica1 {
     public static void main(String[] args) {
         Scanner n1 = new Scanner(System.in);
         int nu, nd,res;
-        System.out.println("Inserte el primer numero");
         nu = n1.nextInt();
-        System.out.println("Inserte el segundo numero");
         nd = n1.nextInt();
         
         res = nu + nd;
         
-        System.out.println(" los numeros fueron: " + nu + "\t" + nd);
-        
-        System.out.println("la suma de los numeros es: " + res);
+  
+        System.out.println(res);
         
     }
 }
