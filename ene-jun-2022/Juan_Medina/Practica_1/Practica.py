babf16175c6ae95a8438e6795eb55bf94e20d513
ghostt71 <ghostt71@hotmail.com>
2022-01-30 15:56:29 -0600

    Practica 1 Juan Carlos Medina Rodríguez

---
+++ ene-jun-2022/Juan_Medina/Practica_1/Practica.py
@@ -0,0 +1,11 @@
+from sqlite3 import enable_shared_cache
+import sys
+
+num1 = int(input("Ingrese el numero X: "))
+num2 = int(input("Ingrese el numero Y: "))
+
+result = (num1+num2)
+print(result)
+
+
+
