3426bd583b96861bffa5b2a4ff50b9121db4a74e
ghostt71 <ghostt71@hotmail.com>
2022-01-31 19:39:39 -0600

    Practica Juan Carlos Medina Rodríguez

--- ene-jun-2022/Juan_Medina/Practica_1/Practica.py
+++ ene-jun-2022/Juan_Medina/Practica_1/Practica.py
@@ -4,6 +4,13 @@ import sys
 num1 = int(input("Ingrese el numero X: "))
 num2 = int(input("Ingrese el numero Y: "))
 
+if num1>-100:
+    print("Numero de x valido")
+
+if num2<100:
+    print("Numero valido")
+
+
 result = (num1+num2)
 print(result)
 
