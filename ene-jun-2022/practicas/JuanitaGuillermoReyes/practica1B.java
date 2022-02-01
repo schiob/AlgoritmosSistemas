a14350f4e7226b5e743e35b135839ec42ad284d4
unknown <juanitaguillermo@uadec.edu.mx>
2022-02-01 00:19:14 -0600

    Practica 1 Juanita Guillermo Reyes

---
+++ ene-jun-2022/practicas/JuanitaGuillermoReyes/practica1B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class practica1B{
+    
+    public static void main (String[] args){
+    Scanner s =new Scanner (System.in);
+    int a,e,c=0;
+    System.out.println("ingresa dos numeros");
+    e= s.nextInt();
+    a= s.nextInt();
+    c= e + a;
+    System.out.println ("la suma es:"+c);
+   }
+}
+
