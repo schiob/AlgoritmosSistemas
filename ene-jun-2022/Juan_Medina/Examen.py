e623b4d4db59721fa55525a2e459bb79a9711430
Juan Medina <carlos.medina@uadec.edu.mx>
2022-03-11 20:15:29 -0600

    Examen Parcial

---
+++ ene-jun-2022/Juan_Medina/Examen.py
@@ -0,0 +1,21 @@
+## Parcial 1: Ejercicio 1 Ordenando Dinos
+
+arr =  ["Parasaurolophus", "Oviraptor",  "Minmi",  "Troodon", "Wannanosaurus"]
+for i in reversed(sorted(arr, key=len)):
+    print(i)
+
+#En caso de ser de mayor a menor se puede acomodar quitando el reversed. 
+
+## Ejercicio 2 
+
+
+n = 6 #Peliculas de Filomeno 
+a = "StarWars 2 1"
+b = "Alien 1 57"
+c = "ASpaceOdyssey"
+
+
+
+
+
+
