beb5eee43d3e3e5366ed859d2e1571b8f06b896f
AnahiGaray <asauceda@uadec.edu.mx>
2022-03-11 21:24:57 -0600

    Parcial1Ejercicio2Parte1

---
+++ ene-jun-2022/AnahiGabrielaSaucedaGaray/Parcial1E2.java
@@ -0,0 +1,52 @@
+
+public class Parcial1E2 {
+private String pelicula;
+private int horas;
+private double minutos;
+
+
+Parcial1E2(String p, int h, double m){
+	this.pelicula=p;
+	this.horas=h;
+	this.minutos=m;
+}
+
+
+
+
+
+public Parcial1E2(String p, int h, int m) {
+	// TODO Auto-generated constructor stub
+}
+
+
+public String getPelicula() {
+	return pelicula;
+}
+
+
+public void setPelicula(String pelicula) {
+	this.pelicula = pelicula;
+}
+
+
+public int getHoras() {
+	return horas;
+}
+
+
+public void setHoras(int horas) {
+	this.horas = horas;
+}
+
+
+public double getMinutos() {
+	return minutos;
+}
+
+
+public void setMinutos(double minutos) {
+	this.minutos = minutos;
+}
+
+}
