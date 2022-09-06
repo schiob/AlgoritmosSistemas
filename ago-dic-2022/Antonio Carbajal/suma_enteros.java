3713e15392203ccb40214edf08f80eed7bb8c23b
jorga <jorgantonio890@gmail.com>
2022-09-05 20:07:45 -0500

    Lol

---
+++ ago-dic-2022/Antonio Carbajal/suma_enteros.java
@@ -0,0 +1,14 @@
+package Actividades;
+
+public class suma_enteros {
+	public static void main (String[]args) {
+		
+		int x=8;
+		int y=10;
+		int R= x+y;
+		
+		System.out.println(R);
+		
+	}
+
+}
