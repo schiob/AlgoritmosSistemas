eeb63e8c7e4ed679122dbf20e8c7abf52b3d1916
OsvaldoC <contreras.o@uadec.edu.mx>
2022-09-26 19:57:04 -0500

    practica4

---
+++ ago-dic-2022/OsvaldoContrerasVega/practica4.php
@@ -0,0 +1,52 @@
+<?php 
+class mergeS {
+    function mergeSort(&$arr){
+        
+        if(count($arr) > 1) {
+            $mid = (count($arr)/2);// sustitui el n/2 por count
+                                   //n era el numero de elementos 
+            
+            $left = array_slice($arr, 0, $mid);
+            $right = array_slice($arr, $mid);
+            
+
+            $this->mergeSort($left);
+            $this->mergeSort($right);
+
+            // ordenamos izq y derecha
+            $i = 0;
+            $j = 0;
+            $k = 0;
+            while(($i < count($left)) && ($j < count($right))) {
+                if($left[$i] < $right[$j]) {
+                    $arr[$k] = $left[$i];
+                    $i++;
+                } else {
+                    $arr[$k] = $right[$j];
+                    $j++;
+                }
+                $k++;
+            }
+
+            while($i < count($left)) {
+                $arr[$k] = $left[$i];
+                $i++;
+                $k++;
+            }
+
+            while($j < count($right)) {
+                $arr[$k] = $right[$j];
+                $j++;
+                $k++;
+            }
+        }
+    }
+}
+
+$s = new mergeS();
+$tmp = ["text2a", "texts", "text2a12", "a", "a12", "text2a2", "b2"];//elementos a ordenar
+$s->mergeSort($tmp);
+print_r($tmp);
+
+//text2a, text2, text2a12, a, a12, text2a2, b2]
+?>
