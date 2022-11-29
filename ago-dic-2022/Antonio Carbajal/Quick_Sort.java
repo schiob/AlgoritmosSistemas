eb0a2b9b3a3821c8eb9adc0bbe4fd248e1e264fb
jorga <jorgantonio890@gmail.com>
2022-11-28 21:14:47 -0600

    Actividad 1 del examen ordinario. NOTA: Hoy le subiré las tareas restatntes por tiempo no podía

--- ago-dic-2022/Antonio Carbajal/Quick_Sort.java
+++ ago-dic-2022/Antonio Carbajal/Quick_Sort.java
@@ -1,140 +1,62 @@
-package Tareas;
-
-import Actividades__P2.Binary_Search;
+package Actividades;
 
 public class Quick_Sort {
-	 static void swap(String[] arr, int i, int j)
-	    {
-	        String temp = arr[i];
-	        arr[i] = arr[j];
-	        arr[j] = temp;
-	    }
-	  
-	    /* This function takes last element as pivot, places
-	       the pivot element at its correct position in sorted
-	       array, and places all smaller (smaller than pivot)
-	       to left of pivot and all greater elements to right
-	       of pivot */
-	    static int partition(String[] arr, int low, int high)
-	    {
-	  
-	        // pivot
-	        String pivot = arr[high];
-	  
-	        // Index of smaller element and
-	        // indicates the right position
-	        // of pivot found so far
-	        int i = (low - 1);
-	  
-	        for (int j = low; j <= high - 1; j++) {
-	  
-	            // If current element is smaller
-	            // than the pivot
-	            if (arr[j].equalsIgnoreCase(pivot)) {
-	  
-	                // Increment index of
-	                // smaller element
-	                i++;
-	                swap(arr, i, j);
-	            }
-	        }
-	        swap(arr, i + 1, high);
-	        return (i + 1);
-	    }
-	  
-	    /* The main function that implements QuickSort
-	              arr[] --> Array to be sorted,
-	              low --> Starting index,
-	              high --> Ending index
-	     */
-	    static void quickSort(String[] arr, int low, int high)
-	    {
-	        if (low < high) {
-	  
-	            // pi is partitioning index, arr[p]
-	            // is now at right place
-	            int pi = partition(arr, low, high);
-	  
-	            // Separately sort elements before
-	            // partition and after partition
-	            quickSort(arr, low, pi - 1);
-	            quickSort(arr, pi + 1, high);
-	        }
-	    }
-	    int binarySearch(String arr[], int l, int r, String x)
-	    {
-	        if (r >= l) {
-	            int mid = l + (r - l) / 2;
-	 
-	            // If the element is present at the
-	            // middle itself
-	            if (arr[mid].equals(x))
-	                return mid;
-	 
-	            // If element is smaller than mid, then
-	            // it can only be present in left subarray
-	            if (arr[mid].equals(x))
-	                return binarySearch(arr, l, mid - 1, x);
-	 
-	            // Else the element can only be present
-	            // in right subarray
-	            return binarySearch(arr, mid + 1, r, x);
-	        }
-	 
-	        // We reach here when element is not present
-	        // in array
-	        return -1;
-	    }
-	  
-	    // Function to print an array
-	    static void printArray(String[] arr, int size)
-	    {
-	        for (int i = 0; i < size; i++)
-	            System.out.print(arr[i] + " ");
-	  
-	        System.out.println();
-	    }
-	  
-	    // Driver Code
-	    public static void main(String[] args)
-	    {
-	    	Quick_Sort ob = new Quick_Sort();
-	        String[] arr = { "FiestaDeLocos", "Matador", "Comprendes,Mendes?", "PadreNuestro", "YoTomo", "Frijolero", "SiSenor", "Imagina" };
-	        int n = arr.length;
-	  
-	        quickSort(arr, 0, n - 1);
-	        System.out.println("Sorted array: ");
-	        printArray(arr, n);
-	        String x = "SiSenor";
-	        String y = "Imagina";
-	        String z = "PadreNuestro";
-	        String w = "Matador";
-	        
-	        int result = ob.binarySearch(arr, 0, n - 1, x);
-	        int result2 = ob.binarySearch(arr, 1, n - 1, y);
-	        int result3 = ob.binarySearch(arr, 2, n - 1, z);
-	        int result4 = ob.binarySearch(arr, 3, n - 1, w);
-	      
-	        if (result == -1)
-	            System.out.println("Element not present");
-	        else
-	            System.out.println("Indice: "
-	                               + result);
-	        if (result2 == -1)
-	            System.out.println("Element not present");
-	        else
-	            System.out.println("Indice:  "
-	                               + result2);
-	        if (result3 == -1)
-	            System.out.println("Element not present");
-	        else
-	            System.out.println("Indice:  "
-	                               + result3);
-	        if (result4 == -1)
-	            System.out.println("Element found at index ");
-	        else
-	            System.out.println("Indice: "
-	                               + result4);
-	    }
+	
+	public void quick(String arr[],int izq, int der) {
+		int j,i;
+		String pivote, auxiliar;
+		i=izq;
+		j=der;
+		pivote=arr[(izq+der)/2];
+		do {
+			while(arr[i].compareTo(pivote)<0) {
+				i++;
+				
+			}while(arr[j].compareTo(pivote)>0) {
+				j--;
+				
+			}//Hacemos el intercambio
+			if(i<=j) {
+				auxiliar=arr[i];
+				arr[i]=arr[j];
+				arr[j]=auxiliar;
+				i++;
+				j--;
+				
+			}
+		}while(i<=j);
+		if(izq<j) {
+			quick(arr,izq,j);
+			
+		}if (i<der) {
+			quick(arr,i,der);
+			
+		}
+		mostrarArreglo(arr);
+		
+	}
+	public void mostrarArreglo(String [] arr) {
+		int k;
+		for(k=0;k<arr.length;k++) {
+			System.out.println("["+arr[k]+"] ");
+			
+		}
+		System.out.println();
+	}
+	public static void main(String[]args) {
+		Quick_Sort ordenar= new Quick_Sort();
+		 String[] nombres = {"EstudiarParaAlgoritmos 64", "CompletarTareasPendientes 54", "RezarPorLosParciales 80", "ComprarLasBotellasDeSobornos 90", "LlorarEnUnRincón 20"};
+		 System.out.println("Arreglo original:  ");
+		 
+		 ordenar.mostrarArreglo(nombres);
+		 
+		 System.out.println("Arreglo ordenado con quick sort:  ");
+		 ordenar.quick(nombres, 0, nombres.length-1);
+		 
+		 
+		 
+	} /*NOTA: Este tambié está imcompleto debido a que se solicite que
+	 se ordene mediante la prioridad de los enteros por lo que en java desconozo 
+	 como s ordenan con los strings*/
 
 }
