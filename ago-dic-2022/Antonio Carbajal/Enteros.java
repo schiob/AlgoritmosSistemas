d82225813a1a0b4b0eef423a8ad28d630271954a
jorga <jorgantonio890@gmail.com>
2023-01-23 19:39:40 -0600

    Practcica 1

---
+++ ago-dic-2022/Antonio Carbajal/Enteros.java
@@ -0,0 +1,18 @@
+package Semestre1;
+
+import java.util.*;
+
+public class Enteros {
+	
+	public static void main(String[]args) {
+	Scanner e=new Scanner(System.in);
+	
+	int e1=e.nextInt();
+	int e2=e.nextInt();
+	int sum= e1+e2;
+	
+	System.out.println("Total= "+sum);
+	}
+	
+
+}
