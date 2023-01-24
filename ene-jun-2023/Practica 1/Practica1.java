8a12b96eb899d7e9eb023cdbc0444a41e7c170ee
Cancino <cancino_victor@uadec.edu.mx>
2023-01-23 20:35:40 -0600

    practica1_Cancino

---
+++ ene-jun-2023/Practica 1/Practica1.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author Victor Cancino
+ */
+    import java.util.*;
+
+public class Practica1 {
+
+
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Numero 1:" );
+        int n1 = in.nextInt();
+        System.out.println("Numero 2:" );
+        int n2 = in.nextInt();
+        
+        System.out.println(n1 +" "+n2 + " = " +(n1+n2));
+
+        
+    
+}
+}
