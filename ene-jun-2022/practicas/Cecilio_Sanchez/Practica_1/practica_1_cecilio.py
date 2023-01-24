c55be8908a0af1c64c75b7b8bdb14889c50e588b
cecilio <cecireicito_@hotmail.com>
2023-01-23 20:14:21 -0600

    Practica_1_cecilio

--- ene-jun-2022/practicas/Cecilio_Sanchez/Practica_1/practica_1_cecilio.py
+++
@@ -1,5 +0,0 @@
-import random
-numero1=sum(random.sample(range(-100,100),1))
-numero2=sum(random.sample(range(-100,100),1))
-
-print(str(numero1+numero2))
---
+++ ene-jun-2023/cecilio/Practica_1.py
@@ -0,0 +1,5 @@
+x=int(5)
+y=int(8)
+z=x+y
+print(x,y)
+print(z)
