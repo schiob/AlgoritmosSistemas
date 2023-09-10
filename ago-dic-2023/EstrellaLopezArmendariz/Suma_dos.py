5c9f101f54aca6cf29a92ee0cac965776800ae64
Estrellarlo <Estrellarmendariz1911@outlook.com>
2023-09-10 17:17:20 -0600

    mi primer practica

---
+++ ago-dic-2023/EstrellaLopezArmendariz/Suma_dos.py
@@ -0,0 +1,10 @@
+Numeros = input("Ingrese dos numeros separados por un espacio: ")
+
+Numeros=Numeros.split()
+
+num1 = float(Numeros[0])
+num2 = float(Numeros[1])
+
+suma = num1 + num2
+
+print("La suma de los números insertados es:",suma)
