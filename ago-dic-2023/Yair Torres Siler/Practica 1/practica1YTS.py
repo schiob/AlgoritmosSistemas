dd6b50ad3801e1e2bbabf03046f47f29747e3a24
YairSiller <yairtorres@uadec.edu.mx>
2023-09-11 19:07:47 -0600

    practica 1

---
+++ ago-dic-2023/Yair Torres Siler/Practica 1/practica1YTS.py
@@ -0,0 +1,9 @@
+entrada = input("Ingresar numeros (Separados con espacio)")
+
+numeros = entrada.split()
+
+num1 = int(numeros[0])
+num2 = int(numeros[1])
+
+print(num1," ",num2)
+print(num1 + num2)
