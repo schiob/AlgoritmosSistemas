c1579ff4f7f54f7a38bd7052ce8c37664ffce4be
BasterThTMx <jesus_monjaras@uadec.edu.mx>
2023-09-14 21:45:03 -0600

    Ahora si cumple con los requisitos

--- ago-dic-2023/jesusMonjaras/practica1.java
+++ ago-dic-2023/jesusMonjaras/practica1.java
@@ -3,15 +3,21 @@ import java.util.Scanner;
 public class practica1 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        
-        System.out.print("Ingrese numero 1: ");
-        int num1 = scn.nextInt();
+        String numerosConEspacio;
 
-        System.out.print("Ingrese numero 2: ");
-        int num2 = scn.nextInt();
+        System.out.print("Ingrese dos numeros separados por un espacio: ");
+        numerosConEspacio = scn.nextLine();
+
+        String numerosSinEspacio[] = numerosConEspacio.split(" ");
+
+        int suma = 0;
+
+        for (int i = 0; i < 2; i++) {
+            suma += Integer.parseInt(numerosSinEspacio[i]);
+        }
+
+        System.out.print("La suma de ambos numeros es: " + suma);
 
         scn.close();
-
-        System.out.println("La suma es: " + (num1 + num2));
     }
 }
