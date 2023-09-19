2983b5205af621d086daaa551f94243671e315d4
sandra <lugos6873@gmail.com>
2023-09-18 19:30:33 -0600

    practica1

---
+++ ago-dic-2023/practicas/practica1/Sandra_Lugo/Actividad_1.java
@@ -0,0 +1,15 @@
+
+package Practicas;
+
+public class Actividad_1 {
+
+    
+    public static void main(String[] args) {
+int A=4;
+   int B=6;
+        System.out.println(A+B);
+        
+
+    }
+    
+}
