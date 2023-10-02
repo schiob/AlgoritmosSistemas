dfa3585b5a80577e4f3da1e1d93a51d9d52a3c85
Alfonso Ramírez Cárdenas <alf.ram.car.1@hotmail.com>
2023-10-02 11:12:00 -0600

    Practica 4 Alfonso Ramírez Cárdenas

---
+++ ago-dic-2023/AlfonsoRamírezCárdenas/Practica4.kt
@@ -0,0 +1,44 @@
+un seleccionSort(arregloPalabras: MutableList<String>) {
+    val n = arregloPalabras.size
+
+    for (i in 0 until n - 1) {
+        var minIndex = i
+
+        // Encuentra el índice de la palabra más pequeña en la lista no ordenada
+        for (j in i + 1 until n) {
+            if (arregloPalabras[j] < arregloPalabras[minIndex]) {
+                minIndex = j
+            }
+        }
+
+        // Intercambia la palabra más pequeña con la palabra en la posición actual
+        val temp = arregloPalabras[i]
+        arregloPalabras[i] = arregloPalabras[minIndex]
+        arregloPalabras[minIndex] = temp
+    }
+}
+
+fun main() {
+    println("Por favor, ingrese el número de palabras:")
+
+    val n = readLine()!!.toInt()
+
+    if (n <= 0) {
+        println("El número de palabras debe ser mayor que cero.")
+        return
+    }
+
+    println("Ahora ingrese las $n palabras separadas por espacios:")
+
+    val palabras = readLine()?.split(" ")?.toMutableList()
+
+    if (palabras == null || palabras.size != n) {
+        println("No ingresó la cantidad correcta de palabras.")
+        return
+    }
+
+    seleccionSort(palabras)
+
+    println("Lista de palabras ordenadas alfabéticamente:")
+    println(palabras.joinToString(" "))
+}
