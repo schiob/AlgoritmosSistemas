f51a8b6b9e1d874c43e13d42d51b2b843ac7b3e0
Brayan <el.el.brayan16mail.com>
2023-10-08 15:24:35 -0600

    Tarea 4 <Corregida

--- ago-dic-2023/Brayan Arias/TAREAS/Tarre_4.py
+++ ago-dic-2023/Brayan Arias/TAREAS/Tarre_4.py
@@ -1,11 +1,10 @@
-# Ordenamiento burbuja
 
-n = int(input())
-words = input().split()
+n = int(input("Numero de palabras en la lista de Ricardo: "))
+words =input("Ingrese los datos: ").split()
 
 for i in range(len(words) - 1):
     for j in range(len(words) - 1 - i):
-        if words[j] > words[j + 1]:
+        if len(words[j]) > len(words[j + 1]):
             pequeño = words[j + 1]
             grande = words[j]
             words[j] = pequeño
