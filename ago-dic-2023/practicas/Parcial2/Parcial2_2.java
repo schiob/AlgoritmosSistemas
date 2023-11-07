76661f0f305de426a9386688bcb366ef1c0d9338
RICKYFCO <rickyfco8@gmail.com>
2023-11-06 20:46:02 -0600

    Parcial2

---
+++ ago-dic-2023/practicas/Parcial2/Parcial2_2.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Parcial2_2 {
+
+    static int getMax(int arr[], int n) {
+        int max = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > max)
+                max = arr[i];
+        return max;
+    }
+
+    static void countSort(int arr[], int n, int exp) {
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        for (int i = 0; i < 10; i++)
+            count[i] = 0;
+
+        for (int i = 0; i < n; i++)
+            count[(arr[i] / exp) % 10]++;
+
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+
+    static void radixSort(int arr[], int n) {
+        int m = getMax(arr, n);
+
+        for (int exp = 1; m / exp > 0; exp *= 10)
+            countSort(arr, n, exp);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        radixSort(arr, n);
+        
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+
+        scanner.close();
+    }
+}
+
