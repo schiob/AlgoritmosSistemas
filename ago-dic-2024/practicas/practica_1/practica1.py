6325accd56f7097b240a3aa193975aa69c994505
Angel200270 <avillalpando@uadec.edu.mx>
2024-09-13 19:59:27 -0600

    practica.py

---
+++ ago-dic-2024/practicas/practica_1/practica1.py
@@ -0,0 +1,10 @@
+entrada = input("Ingresa dos números enteros separados por un espacio: ")
+
+# Dividimos la entrada en dos números
+numero1, numero2 = map(int, entrada.split())
+
+# Calculamos la suma
+suma = numero1 + numero2
+
+# Mostramos el resultado
+print(f"La suma de {numero1} y {numero2} es: {suma}")
