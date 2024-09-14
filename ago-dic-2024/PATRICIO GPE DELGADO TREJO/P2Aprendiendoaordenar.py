c21b6b3603e4a05caf7b583c05a746136d53cb6f
Patricio Delgado <patricio.delgado@uadec.edu.mx>
2024-09-13 20:16:02 -0600

    ahora si

--- ago-dic-2024/PATRICIO GPE DELGADO TREJO/P2Aprendiendoaordenar.py
+++ ago-dic-2024/PATRICIO GPE DELGADO TREJO/P2Aprendiendoaordenar.py
@@ -8,7 +8,7 @@ def insertion_sort(arr):
         arr[j + 1] = key
 
 n, m = map(int, input("2 numeros enteros separados por espacios n1:problemas que tiene, n2:minutos que tiene disponible: ").split())  
-tiempos = list(map(int, input("Tiempo que le toma hacer cada problema: ").split()))  
+tiempos = list(map(int, input("Tiempo que le toma hacer cada problema : ").split()))  
 
 insertion_sort(tiempos)
 
