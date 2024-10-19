4a56194d8b28f21a0980af8012c554bf359ecbcb
Angel200270 <avillalpando@uadec.edu.mx>
2024-10-18 19:22:50 -0600

    practica4.py

---
+++ ago-dic-2024/practica4.py
@@ -0,0 +1,40 @@
+def merge_sort(lst):
+    if len(lst) > 1:
+        mid = len(lst) // 2
+        left_half = lst[:mid]
+        right_half = lst[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                lst[k] = left_half[i]
+                i += 1
+            else:
+                lst[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            lst[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            lst[k] = right_half[j]
+            j += 1
+            k += 1
+
+def main():
+    n = int(input())  
+    words = input().split()  
+    
+    merge_sort(words)
+    
+    print(" ".join(words))
+
+if __name__ == "__main__":
+    main()
